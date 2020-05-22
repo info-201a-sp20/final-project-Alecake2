5e6716bc6c146c65e66bdc7a773e0a7adb3e3dcb
Nur Bahrin <nbahrin@uw.edu>
2020-05-22 10:09:02 -0700

    add percentage into data

--- scripts/aggregrate_table.R
+++ scripts/aggregrate_table.R
@@ -6,8 +6,8 @@ library(dplyr)
 
 covid_cases <- read.csv("../data/us_states_covid19_daily.csv", stringsAsFactors = FALSE)
 
-colnames(covid_cases) #column names
-nrow(covid_cases) #`number of rows
+colnames(covid_cases) # column names
+nrow(covid_cases) # number of rows
 ncol(covid_cases) # number of columns
 
 # function to get info for column
@@ -15,16 +15,23 @@ info_covid <- function(dataframe) {
   summary <- dataframe %>% 
     group_by(state) %>%
     select(positiveIncrease, negativeIncrease, totalTestResultsIncrease) %>% 
-    summarize(mean_positve_case = round(mean(positiveIncrease, na.rm = TRUE),0),
-              total_positive_case = round(sum(positiveIncrease, na.rm = TRUE),0),
-              mean_negative_case = round(mean(negativeIncrease, na.rm = TRUE),0),
-              total_negative_case = round(sum(negativeIncrease, na.rm = TRUE),0),
-              mean_num_cases = round(mean(totalTestResultsIncrease, na.rm = TRUE),0),
-              total_num_cases = round(sum(totalTestResultsIncrease, na.rm = TRUE),0),
-              ) %>% 
-    arrange(-total_num_cases) 
+    summarize(total_positive_case = round(sum(positiveIncrease, na.rm = TRUE), 0),
+              total_negative_case = round(sum(negativeIncrease, na.rm = TRUE), 0),
+              total_testing = round(sum(totalTestResultsIncrease, na.rm = TRUE), 0),
+              percent_positive = round(total_positive_case / total_testing * 100, 2)) %>% 
+    arrange(-percent_positive)
 }
 
 # this table will give out the summary for the testing conducted in the states
 state_testing <- info_covid(covid_cases)
 
+# Check if everything is tally
+state_testing$total_testing == state_testing$total_positive_case + state_testing$total_negative_case
+# Seems like the result for PR has a problem
+
+# Covid for PR
+PR_covid <- covid_cases %>% filter(state == "PR")
+
+covid_check <- all(covid_cases$positiveIncrease + covid_cases$negativeIncrease == covid_cases$totalTestResultsIncrease) == TRUE 
+
+# Data set for covid cases in PR states is not accurate
