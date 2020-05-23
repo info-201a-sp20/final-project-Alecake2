bb3fc5151cd046116b7cd6dc367021a4f4369379
Nur Bahrin <nbahrin@uw.edu>
2020-05-22 21:50:17 -0700

    edit code

--- scripts/aggregrate_table.R
+++ scripts/aggregrate_table.R
@@ -3,29 +3,34 @@
 rm(list = ls())
 
 library(dplyr)
-
-covid_cases <- read.csv("../data/us_states_covid19_daily.csv", stringsAsFactors = FALSE)
+covid_cases <- read.csv("../data/us_states_covid19_daily.csv",
+                        stringsAsFactors = FALSE)
 
 colnames(covid_cases) # column names
 nrow(covid_cases) # number of rows
 ncol(covid_cases) # number of columns
 
 # Check if everything is tally
-check_df <- covid_cases %>% 
-  mutate(answer = covid_cases$positiveIncrease + covid_cases$negativeIncrease == covid_cases$totalTestResultsIncrease) %>% 
+check_df <- covid_cases %>%
+  mutate(answer = covid_cases$positiveIncrease +
+covid_cases$negativeIncrease == covid_cases$totalTestResultsIncrease) %>%
   filter(answer == FALSE)
-# Seems like one of the data entry for PR has a problem because it's not accurate
+# Seems like one of the data entry for PR has a problem because it's
+# not accurate.
 
 # Count the total of testing manually
 # function to get info for column
 info_covid <- function(dataframe) {
-  summary <- dataframe %>% 
+  summary <- dataframe %>%
     group_by(state) %>%
-    select(positiveIncrease, negativeIncrease) %>% 
-    summarize(total_positive_case = round(sum(positiveIncrease, na.rm = TRUE), 0),
-              total_negative_case = round(sum(negativeIncrease, na.rm = TRUE), 0),
+    select(positiveIncrease, negativeIncrease) %>%
+    summarize(total_positive_case = round(sum(positiveIncrease,
+                                              na.rm = TRUE), 0),
+              total_negative_case = round(sum(negativeIncrease,
+                                              na.rm = TRUE), 0),
               total_testing = total_positive_case + total_negative_case,
-              percent_positive = round(total_positive_case / total_testing * 100, 2)) %>%
+              percent_positive = round(
+                total_positive_case / total_testing * 100, 2)) %>%
               arrange(-percent_positive)
 }
 
