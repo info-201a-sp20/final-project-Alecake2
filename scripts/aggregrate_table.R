1632696911543e268f838717469dfa95cf7025af
Nur Bahrin <nbahrin@uw.edu>
2020-05-22 21:44:17 -0700

    fix table

--- scripts/aggregrate_table.R
+++ scripts/aggregrate_table.R
@@ -10,28 +10,24 @@ colnames(covid_cases) # column names
 nrow(covid_cases) # number of rows
 ncol(covid_cases) # number of columns
 
+# Check if everything is tally
+check_df <- covid_cases %>% 
+  mutate(answer = covid_cases$positiveIncrease + covid_cases$negativeIncrease == covid_cases$totalTestResultsIncrease) %>% 
+  filter(answer == FALSE)
+# Seems like one of the data entry for PR has a problem because it's not accurate
+
+# Count the total of testing manually
 # function to get info for column
 info_covid <- function(dataframe) {
   summary <- dataframe %>% 
     group_by(state) %>%
-    select(positiveIncrease, negativeIncrease, totalTestResultsIncrease) %>% 
+    select(positiveIncrease, negativeIncrease) %>% 
     summarize(total_positive_case = round(sum(positiveIncrease, na.rm = TRUE), 0),
               total_negative_case = round(sum(negativeIncrease, na.rm = TRUE), 0),
-              total_testing = round(sum(totalTestResultsIncrease, na.rm = TRUE), 0),
-              percent_positive = round(total_positive_case / total_testing * 100, 2)) %>% 
-    arrange(-percent_positive)
+              total_testing = total_positive_case + total_negative_case,
+              percent_positive = round(total_positive_case / total_testing * 100, 2)) %>%
+              arrange(-percent_positive)
 }
 
 # this table will give out the summary for the testing conducted in the states
 state_testing <- info_covid(covid_cases)
-
-# Check if everything is tally
-state_testing$total_testing == state_testing$total_positive_case + state_testing$total_negative_case
-# Seems like the result for PR has a problem
-
-# Covid for PR
-PR_covid <- covid_cases %>% filter(state == "PR")
-
-covid_check <- all(covid_cases$positiveIncrease + covid_cases$negativeIncrease == covid_cases$totalTestResultsIncrease) == TRUE 
-
-# Data set for covid cases in PR states is not accurate
