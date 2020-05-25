054d1aac2ac7d48622d0281158aa2db32a88713a
Alec Gao <lgao2@uw.edu>
2020-05-24 19:55:36 -0700

    finished third chart

--- scripts/chart_three.R
+++ scripts/chart_three.R
@@ -1,3 +1,24 @@
+library(tidyverse)
+library(scales)
+library(gridExtra)
+library(ggthemes)
 library(dplyr)
+library("plotly")
+library("rbokeh")
 covid_cases <- read.csv("../data/us_states_covid19_daily.csv",
                         stringsAsFactors = FALSE)
+
+df <- covid_cases %>%
+  select(date, positive) %>%
+  group_by(date) %>%
+  summarise(positive = sum(positive))
+
+df$date <- as.character(df$date)
+
+plot_ly(
+  data = df,
+  x = ~date,
+  y = ~positive,
+  type = "scatter",
+  mode = "markers"
+) 
