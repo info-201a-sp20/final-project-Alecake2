572d9d4cfcd6010516d18f91a79db1166318344c
Kavi Gill <kavigill@kavis-mbp.attlocal.net>
2020-05-24 20:12:55 -0700

    added

--- scripts/Summary.R
+++ scripts/Summary.R
@@ -11,30 +11,30 @@ get_summary_info <- function(data_set) {
     max_cases_covid_19 = select(data_set, COVID.19.Deaths) %>%
       filter(State == United_States, Sex = "All Sexes") %>%
       summarize(max_cases = max(COVID.19.Deaths)) %>%
-      pull(COVID-19),
+      pull(max_cases),
     # State with most covid deaths
     state_most_cases_covid_19 = select(data_set, state, COVID.19.Deaths) %>%
       group_by(state) %>%
       summarize(avg_cases = avg(COVID.19.Deaths)) %>%
-      filter(Max_cases = max(avg_cases), state != "United States") %>%
+      filter(Max_cases == max(avg_cases), state != "United States") %>%
       pull(state), 
     # Age group in the US with most covid deaths
     us_age_group_biggest = select(data_set, COVID.19.Deaths, Age.group) %>%
-      filter(State == United_States, age_group != All ages, sex = All Sexes, max_covid = max(COVID.19.Deaths)) %>%
+      filter(State == "United States", age_group != "All ages", sex == "All Sexes", max_covid == max(COVID.19.Deaths)) %>%
       pull(age_group),
    # Max total deaths in the US 
     max_total_death_us = select(data_set, Total.Deaths) %>%
-      filter(State == United_States, sex = All Sexes) %>%
+      filter(State == "United States", sex == "All Sexes") %>%
       summarize(max_cases = max(Total.Deaths)) %>%
       pull(Total.Deaths),
     # Avg covid deaths for males
    avg_covid_deaths_males = select(data_set, COVID.19.Deaths) %>%
-      filter(sex = Male) %>%
+      filter(sex == "Male") %>%
       summarize(avg_male_death = avg(COVID.19.Deaths)) %>%
       pull(avg_male_death),
    # Avg covid deaths for females
    avg_covid_deaths_females = select(data_set, COVID.19.Deaths) %>%
-     filter(sex = Female) %>%
+     filter(sex == "Female") %>%
      summarize(avg_female_death = avg(COVID.19.Deaths)) %>%
      pull(avg_female_death)
       
