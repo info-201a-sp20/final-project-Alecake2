4f3dd1b37b9cb818d82d379f4272b2a37b13f804
RuoluYe <62964977+RuoluYe@users.noreply.github.com>
2020-05-26 01:12:40 -0700

    debug Summary R. Descirption for summary table.

--- scripts/Summary.R
+++ scripts/Summary.R
@@ -17,19 +17,27 @@ get_summary_info <- function(data_set = data_set) {
       group_by(State) %>%
       summarize(avg_cases = mean(COVID.19.Deaths)) %>%
       filter(avg_cases == max(avg_cases), State != "United States") %>%
-      pull(State), 
+      pull(State),
     # Age group in the US with most covid deaths
     us_age_group_biggest = select(data_set, State, Sex, COVID.19.Deaths, Age.group) %>%
-      filter(State == "United States", Age.group != "All ages", 
+      filter(State == "United States", Age.group != "All ages",
              Sex == "All Sexes", COVID.19.Deaths == max(COVID.19.Deaths)) %>%
       pull(Age.group),
-    # Max total deaths in the US 
+    # Max total deaths in the US
     max_total_death_us = select(data_set, State, Sex, Total.Deaths) %>%
       filter(State == "United States", Sex == "All Sexes") %>%
       summarize(Total.Deaths = max(Total.Deaths)) %>%
       pull(Total.Deaths),
+    us_age_group_biggest = data_set %>%
+      filter(State == "United States", Age.group != "All Ages", Sex == "All Sexes", COVID.19.Deaths == max(COVID.19.Deaths)) %>%
+      pull(Age.group),
+   # Max total deaths in the US
+    max_total_death_us = data_set %>%
+      filter(State == "United States", Sex == "All Sexes") %>%
+      summarize(Total.Deaths = max(Total.Deaths)) %>%
+      pull(Total.Deaths),
     # Avg covid deaths for males
-    avg_covid_deaths_males = select(data_set, Sex, COVID.19.Deaths) %>%
+    avg_covid_deaths_males = data_set%>%
       filter(Sex == "Male") %>%
       summarize(avg_male_death = mean(COVID.19.Deaths)) %>%
       pull(avg_male_death),
@@ -39,7 +47,6 @@ get_summary_info <- function(data_set = data_set) {
       summarize(avg_female_death = mean(COVID.19.Deaths)) %>%
       pull(avg_female_death)
   )
-  
+
   return(ret)
 }
-
--- scripts/aggregated_table.R
+++ scripts/aggregated_table.R
@@ -35,33 +35,3 @@ info_covid <- function(dataframe) {
 # this table will give out the summary for the testing conducted in the states
 state_testing <- info_covid(covid_cases)
 
-# Introduces the table, explaining why the particular grouping calculation was performed
-# The table sumamrize total number of testing conducted in the states. 
-# We can also know the percentage of positive cases from the total number of testing in
-# the states. 
-
-# Creates a summarized data frame to include as the table using group_by()
-# Table of state_testing
-
-# Intentionally sorts the table in a relevant way
-# We purposely arrange the table descendingly according to the percentage of 
-# positive cases in the states.
-
-# Only displays relevant columns in the table
-# The table shows the percentage of positive cases from the total number of
-# testing according to states.
-# Should I include the total number for positive, negative,and total testing?
-
-# Displays well formatted column names
-# When inserted the table in index.rmd, need to change the name of the columns
-
-# Successfully renders the table in the report using an appropriate package
-# (e.g., don't just print out the data frame)
-# Put the table using kable function
-
-# Interprets information the table, honing in on important information
-# From the table, we can conclude that NJ has the highest percentage for positive
-# COVID-19 cases among the states.
-# We should notice that even NY has a significantly higher number of testing
-# conducted than NJ, the percentage for positive cases in NY is lower than the NJ.
-# Some states conducted not enough testing for COVID-19.
