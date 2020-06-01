e8f9adc143a34b6bce6979ee5342a39a8d47a6c5
Kavi Gill <kavigill@kavis-mbp.attlocal.net>
2020-05-31 22:28:20 -0700

    added

--- vis_2.r
+++ vis_2.r
@@ -9,14 +9,14 @@ covid_deaths <-
   read.csv("data/Provisional_COVID-19_Death_Counts_by_Sex__Age__and_State.csv",
            stringsAsFactors = FALSE, encoding = "UTF-8"
   )
-most_cases_covid_19 <- data_set %>%
+most_cases_covid_19 <- covid_deaths %>%
   filter(State == "United States") 
   
 
 
 
 # Create Visualizations for Page 2
-var1_vis1 <- sidebarLayout(
+var1_vis2 <- sidebarLayout(
   sidebarPanel(
     selectInput(inputId = "gender" ,
                 label = h1("Choose a gender"),
@@ -33,12 +33,21 @@ var1_vis1 <- sidebarLayout(
     )
   ),
   mainPanel(
-    
+    tags$h1("COVID 19 Deaths by gender"),
+    tags$p(
+      id = "vis1_descrip",
+    "This page aims to display Covid 19 Data. This graph shows the
+    Covid 19 death age groups depending on which gender you
+    choose (You can select which gender from the side bar)",
+    ),
+    plotlyOutput("vis2")
   )
 )
 
 # Vis 2
 vis_2 <- tabPanel(
- 
+  "COVID 19 Cases by Gender",
+  titlePanel("Comparing COVID 19 Cases by gender versus age group"),
+  var1_vis2
 )
 
