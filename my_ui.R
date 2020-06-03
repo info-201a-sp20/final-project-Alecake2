7f9093197222c95a650067b5ae45a1b024c418de
RuoluYe <62964977+RuoluYe@users.noreply.github.com>
2020-06-02 20:03:37 -0700

    overview dataset

--- my_ui.R
+++ my_ui.R
@@ -19,9 +19,9 @@ page_intro <- tabPanel(
   "Introduction",
   titlePanel("Introduction"),
   tagList(intro_paragraph,
-          data_1
-          # data_2,
-          # data_3
+          data_1,
+          data_2,
+          data_3
   )
 )
 
@@ -34,8 +34,26 @@ intro_paragraph <- list(
 
 # dataset 1 description
 data_1 <- list(tags$p("The first question is: what is the percentage of positive result from the total number of testing as date moving on from March to May.",
-    "We will use the dataset us_states_covid19_daily.csv which give the daily count of COVID-19 cases in each day. The link of this dataset is "),
-              tags$a(href="https://www.kaggle.com/sudalairajkumar/covid19-in-usa?select=us_states_covid19_daily.csv", "first dataset")
+    "We will use the dataset us_states_covid19_daily.csv which give the daily count of COVID-19 cases in each day. The link of this dataset is ",
+    tags$a(href="https://www.kaggle.com/sudalairajkumar/covid19-in-usa?select=us_states_covid19_daily.csv", "first dataset"))
+)
+
+data_2 <- list(
+  tags$p("The second question is: What is the death rate in every age group for",
+    "male and female. For this question we use tge provisional COVID-19 Death",
+    "Counts dataset based on states, sex and ages that could help us understand", 
+    "the bigger picture.The link of this dataset is "),
+  tags$a(href="https://www.kaggle.com/paultimothymooney/latitude-and-longitude-for-every-country-and-state",
+         "second dataset")   
+)
+
+data_3 <- list(
+  tags$p("The thrid questin is: What effect does unhealthy habbit have on",
+    "affecting rate. For this question we use a comprehensive one about each",
+    "U.S county, which collects information related to their weather", 
+    "socio/health and COVID-19 situation. Since its size exceeds the",
+    "upload limit, we have cleaned and truncate it. The Link is: "),
+  tags$a(href="https://www.kaggle.com/johnjdavisiv/us-counties-covid19-weather-sociohealth-data", "third dataset")
 )
 
 page_summary <- tabPanel(
