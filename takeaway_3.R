34996e4b57337b9cbbaac5a0e40ad5538255f20a
Alec Gao <lgao2@uw.edu>
2020-06-02 22:33:52 -0700

    takeaway three

---
+++ takeaway_3.R
@@ -0,0 +1,6 @@
+"Base on the data, we don't see any clear association between bad habits and 
+covid-19 death rate. The scatter plots for all five habits(smoking, drinking, 
+physically inactive, and insufficient sleep) appear randomly distributed. 
+However, we can't firmly conclude that there is no association between those
+habits and covid-19 death rate, because the sample size is relatively small when 
+we use county as individual data point."
